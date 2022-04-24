5684c234810a919d880e217e418679c411e7d6d9
陈孝孝 <48433638+c823969351@users.noreply.github.com>
2022-04-24 14:05:22 +0800

    隐藏secret

--- common/dingdingrobot.py
+++ common/dingdingrobot.py
@@ -1,7 +1,7 @@
 from dingtalkchatbot.chatbot import DingtalkChatbot
 
 webhook = 'https://oapi.dingtalk.com/robot/send?access_token=f59535ecc4bd8647c71290957e49efa3b8434ed6f5b3117f617b065d5a6bb03a'
-secret = 'SEC4d9eea43714470401d9d926412188201f39e027f92b8c16caf8a82e2e61fe26f'
+secret = '***'
 
 dingding = DingtalkChatbot(webhook,secret=secret,pc_slide=True,fail_notice=True)
 
